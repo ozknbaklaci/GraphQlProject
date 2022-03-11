eb0bc7d718cc0b05b6097e257a41e211904b303b
ozknbaklaci <ozkan-baklaci@hotmail.com>
2022-03-11 14:13:50 +0300

    Add GraphType

---
+++ GraphQlProject/Type/ProductType.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL.Types;
+using GraphQlProject.Models;
+
+namespace GraphQlProject.Type
+{
+    public class ProductType : ObjectGraphType<Product>
+    {
+        public ProductType()
+        {
+            Field(p => p.Id);
+            Field(p => p.Name);
+            Field(p => p.Price);
+        }
+    }
+}
