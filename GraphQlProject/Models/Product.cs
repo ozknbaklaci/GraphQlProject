522457acc2e8fe92bbd51cc64d8861d329c47fb0
ozknbaklaci <ozkan-baklaci@hotmail.com>
2022-03-11 13:29:57 +0300

    Added Model And Interfaces

---
+++ GraphQlProject/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace GraphQlProject.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+    }
+}
