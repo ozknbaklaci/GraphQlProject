a6e749dda0d9835708ea42d2971c6e6224d4254c
ozknbaklaci <ozkan-baklaci@hotmail.com>
2022-03-12 13:56:20 +0300

    Adding Models For Coffee Shop Project

---
+++ GraphQlProject/Models/CoffeeShop/Menu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace GraphQlProject.Models.CoffeeShop
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public ICollection<SubMenu> SubMenus { get; set; }
+    }
+}
