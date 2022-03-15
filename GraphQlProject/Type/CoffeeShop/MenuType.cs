61080640c9783e564eecf3b70d8eb7f8b44b9a61
ozknbaklaci <ozkan-baklaci@hotmail.com>
2022-03-15 19:44:10 +0300

    Nested Query

--- GraphQlProject/Type/CoffeeShop/MenuType.cs
+++ GraphQlProject/Type/CoffeeShop/MenuType.cs
@@ -1,15 +1,19 @@
 ﻿using GraphQL.Types;
+using GraphQlProject.Interfaces.CoffeeShop;
 using GraphQlProject.Models.CoffeeShop;
 
 namespace GraphQlProject.Type.CoffeeShop
 {
     public class MenuType : ObjectGraphType<Menu>
     {
-        public MenuType()
+        public MenuType(ISubMenuService subMenuService)
         {
             Field(m => m.Id);
             Field(m => m.Name);
             Field(m => m.ImageUrl);
+
+            Field<ListGraphType<SubMenuType>>("submenus",
+                resolve: context => subMenuService.GetSubMenus(context.Source.Id));
         }
     }
 }
