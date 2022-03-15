e566ad514d398998f33aed1eb1b9ad524e8e062b
ozknbaklaci <ozkan-baklaci@hotmail.com>
2022-03-15 18:06:20 +0300

    Create Types

---
+++ GraphQlProject/Type/CoffeeShop/InputTypes/MenuInputType.cs
@@ -0,0 +1,14 @@
+﻿using GraphQL.Types;
+
+namespace GraphQlProject.Type.CoffeeShop.InputTypes
+{
+    public class MenuInputType : InputObjectGraphType
+    {
+        public MenuInputType()
+        {
+            Field<IntGraphType>("id");
+            Field<StringGraphType>("name");
+            Field<StringGraphType>("imageUrl");
+        }
+    }
+}
---
+++ GraphQlProject/Type/CoffeeShop/InputTypes/SubMenuInputType.cs
@@ -0,0 +1,17 @@
+﻿using GraphQL.Types;
+
+namespace GraphQlProject.Type.CoffeeShop.InputTypes
+{
+    public class SubMenuInputType : InputObjectGraphType
+    {
+        public SubMenuInputType()
+        {
+            Field<IntGraphType>("id");
+            Field<StringGraphType>("name");
+            Field<StringGraphType>("imageUrl");
+            Field<StringGraphType>("description");
+            Field<FloatGraphType>("price");
+            Field<IntGraphType>("menuId");
+        }
+    }
+}
